4258a10a77c58fb5655c52340399363a097afecf
theprivatepark <anneypark@gmail.com>
2020-08-29 23:45:39 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    # self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count #working method found on SOF but it's too complicated for me right now to understand
+    self.split(/[.?!]+/).count
 
   end
 end
